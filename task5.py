6841934cc0288ce510d5796969a21c3b3260e858
nikitakom2004 <nikitakomagin342@gmail.com>
2024-12-07 15:35:00 +0300

    Add files via upload

---
+++ task5.py
@@ -0,0 +1,8 @@
+x=input("Введите сумму")
+y=input("Введите цену товара")
+x1=float(x)
+y1=float(y)
+n=int(x1/y1)
+s=x1%y1
+print(n)
+print(s)
---
+++ task6.py
@@ -0,0 +1,9 @@
+F=405.9
+K=1000.0
+
+vf=input("Введите вес в фунтах")
+vf1=float(vf)
+vk=vf1*F/K
+print(vk)
+vf2=vk*K/F
+print(vf2)
