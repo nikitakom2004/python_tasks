709994e1ff2734baa7553d84113a8d6f9a3c3818
Nikita Komyagin <nikitakomagin342@gmail.com>
2025-02-16 12:09:02 +0300

    Create task5_1.py

---
+++ unit5/task5_1.py
@@ -0,0 +1,8 @@
+x1= int(input('x1='))
+y1= int(input('y1='))
+x2= int(input('x2='))
+y2= int(input('y2='))
+if y1==y2:
+    print("прямая параллельна оси х")
+if x1==x2:
+    print("прямая параллельна оси у")
